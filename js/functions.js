36734c1aed45b71700f6dcc11c3869f3cf763a29
Juli Ramon <juli@nexumlabs.com>
2021-09-16 18:36:05 +0200

    Implement scroll down functionality

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,13 @@
-$(document).ready(() => {
-  $(".intro .slick").slick({
-    infinite: false,
-    dots: false,
-    slidesToShow: 1,
-  });
-});
+const scrollDownButton = document.getElementById("scrollDownBtn");
+const servicesSection = document.getElementById("services");
+scrollDownButton.addEventListener("click", () =>
+  servicesSection.scrollIntoView()
+);
+
+// $(document).ready(() => {
+//   $(".intro .slick").slick({
+//     infinite: false,
+//     dots: false,
+//     slidesToShow: 1,
+//   });
+// });
